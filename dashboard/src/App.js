bbf1a37b7e7e836cba648b36d0ea396dbe502e72
Lasantha Basnayake <lasantha@huubap.com>
2019-12-07 02:09:31 +0800

    Change bg color

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
 
     render() {
         return (
-            <Container className="App">
+            <Container className="App bg-white">
 
                 <Router>
                     {this.renderHeader()}
